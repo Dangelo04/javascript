af2fd7d5eb67f178bf571fd4e867a034fe4474e9
Dangelo04 <128734552+Dangelo04@users.noreply.github.com>
2023-08-29 19:41:38 -0300

    mudança de pasta

---
+++ aula11/exe008.js
@@ -0,0 +1,7 @@
+var vel = 80.5
+console.log(`A velocidade do meu carro é de ${vel}KM/h`)
+if (vel > 60){//condição simples!
+    console.log(`você ultrapassou a velocidade permitida.MULTADO!`)
+
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
